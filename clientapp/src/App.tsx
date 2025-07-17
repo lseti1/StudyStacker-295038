d91a12cc1eb38c0c05b65092dbe5bc8c3c90021f
Lucas Setiady <luca.setia@gmail.com>
2025-07-17 17:04:56 +1000

    Files: Set up initial app, using React & TypeScript Frontend with Tailwind CSS (Backend functionality to be added later)

---
+++ clientapp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='w-full bg-blue-400'>
+      <div className=''>
+        <p className='text-4xl'>Tailwind Works</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
